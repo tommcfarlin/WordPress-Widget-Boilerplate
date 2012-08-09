b0a6ff2a835b658f5ed6e23adcf42db10b4e09e0
Amaury Balmer <amaury@beapi.fr>
2012-08-09 18:14:46 +0300

    Update views/admin.php

--- views/admin.php
+++ views/admin.php
@@ -1,6 +1,6 @@
 <!-- This file is used to markup the administration form of the widget. -->
 <p>
-  <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'widget-name-locale' ) ?></label>
+	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'widget-name-locale' ) ?></label>
 	<br/>
 	<input type="text" class="widefat" value="<?php echo esc_attr( $instance['title'] ); ?>" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" />
 </p>
