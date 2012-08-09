987b7afe0d398eb13c103f7f910e6256bac39ff7
Amaury Balmer <amaury@beapi.fr>
2012-08-09 18:16:14 +0300

    Update views/widget.php

--- views/widget.php
+++ views/widget.php
@@ -1,7 +1,7 @@
 <!-- This file is used to markup the public facing widget. -->
 <?php 
-if( isset( $instance['title'] ) && !empty( $instance['title'] ) )
-  echo $before_title.$instance['title'].$after_title;
+if( isset($title) && !empty($title) )
+  echo $before_title.$title.$after_title;
   
 // TODO Build the widget HTLML
 ?>
