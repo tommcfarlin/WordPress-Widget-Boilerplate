7427cea5bc97a94f6ab8c4505787bc327bb3de7f
Tom McFarlin <tom@tommcfarlin.com>
2012-11-02 18:22:22 -0400

    * removing the &this for later php compatibility * updating a call to esc_attr_e from esc_attr

--- views/admin.php
+++ views/admin.php
@@ -2,5 +2,5 @@
 <!-- Note that the use of the 'Title' field is purely for example purposes only. -->
 <p>
 	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'widget-name-locale' ) ?></label>
-	<input type="text" class="widefat" value="<?php echo esc_attr( $instance['title'] ); ?>" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" />
+	<input type="text" class="widefat" value="<?php esc_attr_e( $instance['title'] ); ?>" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" />
 </p>
