86e2c5b3475d0c8695b3d44fbbe38f9f8aaf206e
Ted Waller <ted@tbwiii.com>
2013-04-02 07:57:02 -0400

    Listed jQuery as a dependency for both JavaScripts

--- widget-boilerplate/plugin.php
+++ widget-boilerplate/plugin.php
@@ -16,7 +16,7 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Copyright 2012 TODO (email@domain.com)
 
 This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2, as 
+it under the terms of the GNU General Public License, version 2, as
 published by the Free Software Foundation.
 
 This program is distributed in the hope that it will be useful,
@@ -35,20 +35,20 @@ class Widget_Name extends WP_Widget {
 	/*--------------------------------------------------*/
 	/* Constructor
 	/*--------------------------------------------------*/
-	
+
 	/**
-	 * Specifies the classname and description, instantiates the widget, 
+	 * Specifies the classname and description, instantiates the widget,
 	 * loads localization files, and includes necessary stylesheets and JavaScript.
 	 */
 	public function __construct() {
-	
+
 		// load plugin text domain
 		add_action( 'init', array( $this, 'widget_textdomain' ) );
-		
+
 		// Hooks fired when the Widget is activated and deactivated
 		register_activation_hook( __FILE__, array( $this, 'activate' ) );
 		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
-		
+
 		// TODO:	update classname and description
 		// TODO:	replace 'widget-name-locale' to be named more plugin specific. Other instances exist throughout the code, too.
 		parent::__construct(
@@ -59,21 +59,21 @@ class Widget_Name extends WP_Widget {
 				'description'	=>	__( 'Short description of the widget goes here.', 'widget-name-locale' )
 			)
 		);
-		
+
 		// Register admin styles and scripts
 		add_action( 'admin_print_styles', array( $this, 'register_admin_styles' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_scripts' ) );
-	
+
 		// Register site styles and scripts
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_widget_styles' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_widget_scripts' ) );
-		
+
 	} // end constructor
 
 	/*--------------------------------------------------*/
 	/* Widget API Functions
 	/*--------------------------------------------------*/
-	
+
 	/**
 	 * Outputs the content of the widget.
 	 *
@@ -81,19 +81,19 @@ class Widget_Name extends WP_Widget {
 	 * @param	array	instance	The current instance of the widget
 	 */
 	public function widget( $args, $instance ) {
-	
+
 		extract( $args, EXTR_SKIP );
-		
+
 		echo $before_widget;
-    
+
 		// TODO:	Here is where you manipulate your widget's values based on their input fields
-    
+
 		include( plugin_dir_path( __FILE__ ) . '/views/widget.php' );
-		
+
 		echo $after_widget;
-		
+
 	} // end widget
-	
+
 	/**
 	 * Processes the widget's options to be saved.
 	 *
@@ -101,48 +101,48 @@ class Widget_Name extends WP_Widget {
 	 * @param	array	old_instance	The new instance of values to be generated via the update.
 	 */
 	public function update( $new_instance, $old_instance ) {
-	
+
 		$instance = $old_instance;
-		
+
 		// TODO:	Here is where you update your widget's old values with the new, incoming values
-    
+
 		return $instance;
-		
+
 	} // end widget
-	
+
 	/**
 	 * Generates the administration form for the widget.
 	 *
 	 * @param	array	instance	The array of keys and values for the widget.
 	 */
 	public function form( $instance ) {
-	
+
     	// TODO:	Define default values for your variables
 		$instance = wp_parse_args(
 			(array) $instance
 		);
-	
+
 		// TODO:	Store the values of the widget in their own variable
-		
+
 		// Display the admin form
-		include( plugin_dir_path(__FILE__) . '/views/admin.php' );	
-		
+		include( plugin_dir_path(__FILE__) . '/views/admin.php' );
+
 	} // end form
 
 	/*--------------------------------------------------*/
 	/* Public Functions
 	/*--------------------------------------------------*/
-	
+
 	/**
 	 * Loads the Widget's text domain for localization and translation.
 	 */
 	public function widget_textdomain() {
-	
+
 		// TODO be sure to change 'widget-name' to the name of *your* plugin
 		load_plugin_textdomain( 'widget-name-locale', false, plugin_dir_path( __FILE__ ) . '/lang/' );
-		
+
 	} // end widget_textdomain
-	
+
 	/**
 	 * Fired when the plugin is activated.
 	 *
@@ -151,57 +151,57 @@ class Widget_Name extends WP_Widget {
 	public function activate( $network_wide ) {
 		// TODO define activation functionality here
 	} // end activate
-	
+
 	/**
 	 * Fired when the plugin is deactivated.
 	 *
-	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
+	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
 	 */
 	public function deactivate( $network_wide ) {
-		// TODO define deactivation functionality here		
+		// TODO define deactivation functionality here
 	} // end deactivate
-	
+
 	/**
 	 * Registers and enqueues admin-specific styles.
 	 */
 	public function register_admin_styles() {
-	
+
 		// TODO:	Change 'widget-name' to the name of your plugin
 		wp_enqueue_style( 'widget-name-admin-styles', plugins_url( 'widget-name/css/admin.css' ) );
-	
+
 	} // end register_admin_styles
 
 	/**
 	 * Registers and enqueues admin-specific JavaScript.
-	 */	
+	 */
 	public function register_admin_scripts() {
-	
+
 		// TODO:	Change 'widget-name' to the name of your plugin
-		wp_enqueue_script( 'widget-name-admin-script', plugins_url( 'widget-name/js/admin.js' ) );
-		
+		wp_enqueue_script( 'widget-name-admin-script', plugins_url( 'widget-name/js/admin.js' ), array('jquery') );
+
 	} // end register_admin_scripts
-	
+
 	/**
 	 * Registers and enqueues widget-specific styles.
 	 */
 	public function register_widget_styles() {
-	
+
 		// TODO:	Change 'widget-name' to the name of your plugin
 		wp_enqueue_style( 'widget-name-widget-styles', plugins_url( 'widget-name/css/widget.css' ) );
-		
+
 	} // end register_widget_styles
-	
+
 	/**
 	 * Registers and enqueues widget-specific scripts.
 	 */
 	public function register_widget_scripts() {
-	
+
 		// TODO:	Change 'widget-name' to the name of your plugin
-		wp_enqueue_script( 'widget-name-script', plugins_url( 'widget-name/js/widget.js' ) );
-		
+		wp_enqueue_script( 'widget-name-script', plugins_url( 'widget-name/js/widget.js' ), array('jquery') );
+
 	} // end register_widget_scripts
-	
+
 } // end class
 
 // TODO:	Remember to change 'Widget_Name' to match the class name definition
-add_action( 'widgets_init', create_function( '', 'register_widget("Widget_Name");' ) ); 
+add_action( 'widgets_init', create_function( '', 'register_widget("Widget_Name");' ) );
