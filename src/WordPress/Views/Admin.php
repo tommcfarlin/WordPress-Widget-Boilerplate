f380ef6af1b56917978f15a87e80b224111f1c60
Tom McFarlin <tom@tommcfarlin.com>
2019-02-06 10:06:20 -0500

    Adding the new WidgetAdmin class.

--- src/WordPress/Views/Admin.php
+++ src/WordPress/Views/Admin.php
@@ -1,1 +1,28 @@
-<!-- This file is used to markup the administration form of the widget. -->
+<div class="widget-content">
+    <p>
+        <input
+            type="text"
+            name="widget-title"
+            value=""
+            placeholder="Widget Title"
+            class="widefat"
+        />
+    </p>
+
+    <p>
+        <textarea
+            name="widget-content"
+            placeholder="Widget Content"
+            style="width:100%;"></textarea>
+    </p>
+
+    <p>
+        <input
+            type="checkbox"
+            value="1"
+            name="widget-title-enabled"
+            class="checkbox"
+        />
+        <label for="widget-title-enabled">Display Title?</label>
+    </p>
+</div><!-- .widget-content -->
---
+++ src/WordPress/WidgetAdmin.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of WordPress Widget Boilerplate
+ * (c) Tom McFarlin <tom@tommcfarlin.com>
+ *
+ * This source file is subject to the GPL license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace WordPressWidgetBoilerplate\WordPress;
+
+/**
+ * Manages the administrative functionality of the widget.
+ */
+class WidgetAdmin extends Widget
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($widgetSlug)
+    {
+        parent::__construct($widgetSlug);
+    }
+
+    /**
+     * Displays the administrative view of the form and includes the options
+     * for the instance of the widget as arguments passed into the function.
+     *
+     * @param array $instance the options for the instance of this widget
+     */
+    public function form($instance)
+    {
+        include plugin_dir_path(__FILE__).'Views/Admin.php';
+    }
+}
